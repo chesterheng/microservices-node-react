782f0a107d9e3ee7f231ca984ba01e25cea927a9
Chester Heng <chester.heng@gmail.com>
2020-07-11 16:20:23 +0800

    46. Event Syncing in Action

--- section-02/blog/query/index.js
+++ section-02/blog/query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -47,7 +48,7 @@ app.post('/events', (req, res) => {
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Listening on 4002');
 
   const res = await axios.get('http://localhost:4005/events');
