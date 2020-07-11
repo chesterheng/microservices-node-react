712f095cef02c191c72167e49a25439a5d59e0ee
Chester Heng <chester.heng@gmail.com>
2020-07-11 15:42:39 +0800

    42. A Quick Test

--- section-02/blog/comments/index.js
+++ section-02/blog/comments/index.js
@@ -51,7 +51,7 @@ app.post('/events', async (req, res) => {
     });
     comment.status = status;
 
-    await axios.post('http://localhost:4005', {
+    await axios.post('http://localhost:4005/events', {
       type: 'CommentUpdated',
       data: {
         id,
--- section-02/blog/query/index.js
+++ section-02/blog/query/index.js
@@ -28,6 +28,18 @@ app.post('/events', (req, res) => {
     post.comments.push({ id, content, status });
   }
 
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find(comment => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+
   console.log(posts);
 
   res.send({});
