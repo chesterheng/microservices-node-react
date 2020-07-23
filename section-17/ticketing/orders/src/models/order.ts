002d4c3416b8cb55ef02e0710236b01eaf42afaa
Chester Heng <chester.heng@gmail.com>
2020-07-23 17:04:49 +0800

    324. Order Model Setup

---
+++ section-17/ticketing/orders/src/models/order.ts
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+
+interface OrderAttrs {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+
+export { Order };
