9b52b931afe5963ac13c145576c382a8a13f1ca2
Chester Heng <chester.heng@gmail.com>
2020-07-25 22:08:12 +0800

    426. Testing Order Validation Before Payment

--- section-21.md
+++ section-21.md
@@ -367,6 +367,40 @@ export { router as createChargeRouter };
 **[⬆ back to top](#table-of-contents)**
 
 ### Testing Order Validation Before Payment
+
+```typescript
+it('returns a 404 when purchasing an order that does not exist', async () => {
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin())
+    .send({
+      token: 'asldkfj',
+      orderId: mongoose.Types.ObjectId().toHexString(),
+    })
+    .expect(404);
+});
+
+it('returns a 401 when purchasing an order that doesnt belong to the user', async () => {
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    userId: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin())
+    .send({
+      token: 'asldkfj',
+      orderId: order.id,
+    })
+    .expect(401);
+});
+```
+
 **[⬆ back to top](#table-of-contents)**
 
 ### Testing Same-User Validation
---
+++ section-21/ticketing/payments/src/routes/__test__/new.test.ts
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { OrderStatus } from '@chticketing/common';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+
+it('returns a 404 when purchasing an order that does not exist', async () => {
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin())
+    .send({
+      token: 'asldkfj',
+      orderId: mongoose.Types.ObjectId().toHexString(),
+    })
+    .expect(404);
+});
+
+it('returns a 401 when purchasing an order that doesnt belong to the user', async () => {
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    userId: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin())
+    .send({
+      token: 'asldkfj',
+      orderId: order.id,
+    })
+    .expect(401);
+});
+
+it('returns a 400 when purchasing a cancelled order', async () => {});
