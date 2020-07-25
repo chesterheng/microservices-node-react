3c1d2330523af06e03f9b623bc831aeb2ea4b974
Chester Heng <chester.heng@gmail.com>
2020-07-25 14:07:00 +0800

    375. Testing the Ticket Updated Listener

--- section-19.md
+++ section-19.md
@@ -553,6 +553,40 @@ it('acks the message', async () => {
 **[⬆ back to top](#table-of-contents)**
 
 ### Testing the Ticket Updated Listener
+
+```typescript
+const setup = async () => {
+  // Create a listener
+  const listener = new TicketUpdatedListener(natsWrapper.client);
+
+  // Create and save a ticket
+  const ticket = Ticket.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  // Create a fake data object
+  const data: TicketUpdatedEvent['data'] = {
+    id: ticket.id,
+    version: ticket.version + 1,
+    title: 'new concert',
+    price: 999,
+    userId: 'ablskdjf',
+  };
+
+  // Create a fake msg object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  // return all of this stuff
+  return { msg, data, ticket, listener };
+};
+```
+
 **[⬆ back to top](#table-of-contents)**
 
 ### Success Case Testing
---
+++ section-19/ticketing/orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { TicketUpdatedEvent } from '@chticketing/common';
+import { TicketUpdatedListener } from '../ticket-updated-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Ticket } from '../../../models/ticket';
+
+const setup = async () => {
+  // Create a listener
+  const listener = new TicketUpdatedListener(natsWrapper.client);
+
+  // Create and save a ticket
+  const ticket = Ticket.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  // Create a fake data object
+  const data: TicketUpdatedEvent['data'] = {
+    id: ticket.id,
+    version: ticket.version + 1,
+    title: 'new concert',
+    price: 999,
+    userId: 'ablskdjf',
+  };
+
+  // Create a fake msg object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  // return all of this stuff
+  return { msg, data, ticket, listener };
+};
+
+it('finds, updates, and saves a ticket', async () => {});
+
+it('acks the message', async () => {});
